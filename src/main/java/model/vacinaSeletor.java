0223291312a422dd3c5b6eb54c05a5e2b09cf08b
nathan <nathanping73@gmail.com>
2024-04-15 11:05:07 -0300

    VacinaSeletorCriado

---
+++ src/main/java/model/vacinaSeletor.java
@@ -0,0 +1,49 @@
+package model;
+
+public class vacinaSeletor {
+	
+	private String nomePais;
+	private String nomeVacina;
+	private String nomePesquisador;
+	
+	public vacinaSeletor(String nomePais, String nomeVacina, String nomePesquisador) {
+		super();
+		this.nomePais = nomePais;
+		this.nomeVacina = nomeVacina;
+		this.nomePesquisador = nomePesquisador;
+	}
+
+	public vacinaSeletor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNomePais() {
+		return nomePais;
+	}
+
+	public void setNomePais(String nomePais) {
+		this.nomePais = nomePais;
+	}
+
+	public String getNomeVacina() {
+		return nomeVacina;
+	}
+
+	public void setNomeVacina(String nomeVacina) {
+		this.nomeVacina = nomeVacina;
+	}
+
+	public String getNomePesquisador() {
+		return nomePesquisador;
+	}
+
+	public void setNomePesquisador(String nomePesquisador) {
+		this.nomePesquisador = nomePesquisador;
+	}
+	
+	
+	
+
+}
+
